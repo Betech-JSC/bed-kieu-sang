b584eb73045e0b0857791231489937e2f414e63d
Nguyễn Lê Nhật Bào <bao.nguyenlenhat@h2-inc.com>
2026-06-17 15:15:56 +0700

    fix CORS

--- be/config/cors.php
+++ be/config/cors.php
@@ -5,7 +5,9 @@ return [
     'allowed_methods' => ['*'],
     'allowed_origins' => [
         env('FRONTEND_URL', 'https://xongnhatayue.vn'),
+        env('FRONTEND_WWW_URL', 'https://www.xongnhatayue.vn'),
         env('CMS_URL', 'https://cms.xongnhatayue.vn'),
+        env('CMS_WWW_URL', 'https://www.cms.xongnhatayue.vn'),
         'http://localhost:3000',
         'http://127.0.0.1:3000',
     ],
